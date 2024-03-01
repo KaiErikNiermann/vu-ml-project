585abbd9928e8436ae86caaec6b2675f997fc978
KaiErikNiermann <kai.niermann@gmail.com>
2024-03-01 06:09:43 +0100

    dep fixes

--- src/config.py
+++ src/config.py
@@ -1,2 +1,31 @@
 class config:
-    pass
+    """
+    ### configuration 
+    This is the main configuration for our testing, its based in the recommended configuration from the kaggle 
+    - source : https://www.kaggle.com/code/awsaf49/planttraits2024-kerascv-starter-notebook
+    """
+    verbose = 1                 # Verbosity
+    seed = 42                   # Random seed
+    image_size = [224, 224]     # Input image size
+    epochs = 12                 # Training epochs
+    batch_size = 96             # Batch size
+    lr_mode = "step"            # LR scheduler mode from one of "cos", "step", "exp"
+    drop_remainder = True       # Drop incomplete batches
+    num_classes = 6             # Number of classes in the dataset
+    num_folds = 5               # Number of folds to split the dataset
+    fold = 0                    # Which fold to set as validation data
+
+    class_names = [
+        "X4_mean",
+        "X11_mean",
+        "X18_mean",
+        "X26_mean",
+        "X50_mean",
+        "X3112_mean",
+    ]
+    
+    aux_class_names = list(map(lambda x: x.replace("mean", "sd"), class_names))
+    num_classes = len(class_names)
+    aux_num_classes = len(aux_class_names)
+    
+    
