35bfadf7db41ae35fe0ffbde14716a39d3f13393
KaiErikNiermann <kai.niermann@gmail.com>
2024-03-16 19:36:09 +0100

    added a bunch of stuff

--- src/model.py
+++ src/model.py
@@ -1,41 +1,43 @@
-# deep learning
 import keras_cv
-import keras
-from keras import layers
-from loss import *
+import tensorflow.keras as keras
+from tensorflow.keras import layers
+from loss import R2Loss, R2Metric
+import config
 
-class Model(keras.Model):
-    def __init__(self, config, slct_backbone):
-        super(Model, self).__init__()
-        backbones = {
-            "efficientnetv2_b2_imagenet": keras_cv.models.EfficientNetV2B2Backbone,
-            "efficientnetv2_s_imagenet": keras_cv.models.EfficientNetV2SBackbone,
-            "resnet50_imagenet": keras_cv.models.ResNet50Backbone,
-        }
+def Model(config, slct_backbone):
+    backbones = {
+        "efficientnetv2_b2_imagenet": keras_cv.models.EfficientNetV2B2Backbone,
+        "efficientnetv2_s_imagenet": keras_cv.models.EfficientNetV2SBackbone,
+        "resnet50_imagenet": keras_cv.models.ResNet50Backbone,
+    }
 
-        # Define input layer
-        self.img_input = keras.Input(shape=(*config.image_size, 3), name="images")
+    # Define input layer
+    img_input = keras.Input(shape=(*config.image_size, 3), name="images")
 
-        # Branch for image input
-        backbone = backbones[slct_backbone](slct_backbone)
-        x1 = backbone(self.img_input)
-        x1 = layers.GlobalAveragePooling2D()(x1)
-        x1 = layers.Dense(512, activation="relu")(x1)
-        x1 = layers.Dropout(0.2)(x1)
+    # Branch for image input
+    backbone = backbones[slct_backbone](slct_backbone)
+    backbone_output = backbone(img_input)
+    gavrg1 = layers.GlobalAveragePooling2D()(backbone_output)
+    dense1 = layers.Dense(512, activation="relu")(gavrg1)
+    dropout = layers.Dropout(0.2)(dense1)
 
-        # Output layer
-        out1 = layers.Dense(config.num_classes, activation=None, name="head")(x1)
-        out2 = layers.Dense(config.aux_num_classes, activation="relu", name="aux_head")(x1)
+    # Output layers
+    head_out = layers.Dense(config.num_classes, activation=None, name="head")(dropout)
+    aux_head_out = layers.Dense(config.aux_num_classes, activation="relu", name="aux_head")(dropout)
 
-        self.model = keras.Model(inputs=self.img_input, outputs={"head": out1, "aux_head": out2})
+    model = keras.Model(inputs=img_input, outputs={"head": head_out, "aux_head": aux_head_out})
 
-    def compile_model(self):
-        self.model.compile(
-            optimizer=keras.optimizers.Adam(learning_rate=1e-3),
-            loss={"head": R2Loss(), "aux_head": R2Loss()},
-            loss_weights={"head": 1.0, "aux_head": 0.4},
-            metrics={"head": R2Metric()}
-        )
+    # Compile model
+    print("Compiling model")
+    model.compile(
+        optimizer=keras.optimizers.Adam(learning_rate=1e-3),
+        loss={"head": R2Loss(), "aux_head": R2Loss()},
+        loss_weights={"head": 1.0, "aux_head": 0.4},
+        metrics={"head": R2Metric()},
+    )
 
-    def summary(self):
-        self.model.summary()
+    return model
+
+def model_summary(model):
+    model.build((None, *config.image_size, 3))
+    model.summary()
